9ae079355d0a21e11ecd507378259aab60884e58
Alexey Strelnikov <strelkov@me.com>
2022-11-15 11:51:21 +0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Alexey Strelnikov on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
